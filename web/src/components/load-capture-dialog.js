76b7f4762be86ecffdfdad11812858251c3e0662
Ivan Šakić <ivan.sakic3@gmail.com>
2017-07-08 14:59:11 +0200

    Add sorting to capture/map dialogs
    closes #2
    closes #3

--- web/src/components/load-capture-dialog.js
+++ web/src/components/load-capture-dialog.js
@@ -5,29 +5,76 @@ import { ModalDialog } from './modal-dialog.js'
 
 import styles from './load-dialog.css'
 
-export function LoadCaptureDialog ({open, entries, loadCapture, onClose}) {
-  return <ModalDialog title="Load mission capture" open={open} onClose={onClose}>
-    <table className={styles.captureList}>
-      <thead className={styles.captureListHeader}>
-        <th className={styles.captureListHeaderItem}>Mission</th>
-        <th className={styles.captureListHeaderItem}>World</th>
-        <th className={styles.captureListHeaderItem}>Duration</th>
-        <th className={styles.captureListHeaderItem}>Date</th>
-      </thead>
-      <tbody className={styles.captureList}>
-        {entries.map(entry  =>
+export class LoadCaptureDialog extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      sortBy: 'date',
+      ascending: false,
+    }
+  }
+
+  setSortBy (newSortBy) {
+    const {sortBy, ascending} = this.state
+
+    if (sortBy === newSortBy) {
+      this.setState({
+        ascending: !ascending,
+      })
+    } else {
+      this.setState({
+        sortBy: newSortBy,
+      })
+    }
+  }
+
+  render () {
+    const {open, entries, loadCapture, onClose} = this.props
+    const {sortBy, ascending} = this.state
+
+    return <ModalDialog title="Load mission capture" open={open} onClose={onClose}>
+      <table className={styles.captureList}>
+        <thead className={styles.captureListHeader}>
+        <HeaderCell setSortBy={this.setSortBy.bind(this)} sortKey={'missionName'} label={'Mission'} sortBy={sortBy}
+                    ascending={ascending}/>
+        <HeaderCell setSortBy={this.setSortBy.bind(this)} sortKey={'worldDisplayName'} label={'World'} sortBy={sortBy}
+                    ascending={ascending}/>
+        <HeaderCell setSortBy={this.setSortBy.bind(this)} sortKey={'duration'} label={'Duration'} sortBy={sortBy}
+                    ascending={ascending}/>
+        <HeaderCell setSortBy={this.setSortBy.bind(this)} sortKey={'date'} label={'Date'} sortBy={sortBy}
+                    ascending={ascending}/>
+        </thead>
+        <tbody className={styles.captureList}>
+        {sortListBy(entries, sortBy, ascending).map(entry =>
           <CaptureListItem key={entry.date} entry={entry} onClick={() => loadCapture(entry)}/>
         )}
-      </tbody>
-    </table>
-  </ModalDialog>
+        </tbody>
+      </table>
+    </ModalDialog>
+  }
 }
 
-function CaptureListItem ({entry: {missionName, worldName, duration, date}, onClick}) {
+function CaptureListItem ({entry: {missionName, worldDisplayName, duration, date}, onClick}) {
   return <tr onClick={onClick} className={styles.captureListItem}>
     <td className={styles.captureListItemProperty}>{missionName}</td>
-    <td className={styles.captureListItemProperty}>{worldName}</td>
+    <td className={styles.captureListItemProperty}>{worldDisplayName}</td>
     <td className={styles.captureListItemProperty}>{moment.utc(duration * 1000).format('HH:mm:ss')}</td>
     <td className={styles.captureListItemProperty}>{moment.unix(date).format('DD.MM.YYYY')}</td>
   </tr>
+}
+
+function HeaderCell ({sortKey, setSortBy, label, sortBy, ascending}) {
+  const active = sortKey === sortBy
+  const sortSymbol = active ? (ascending ? '▴' : '▾') : ' '
+  return <th className={styles.captureListHeaderItem} onClick={() => setSortBy(sortKey)}>{label} {sortSymbol}</th>
+}
+
+function sortListBy (list, key, ascending) {
+  const sortModifier = ascending ? 1 : -1
+  return [...list].sort((a, b) => {
+    if (a[key] > b[key]) return sortModifier
+    if (a[key] < b[key]) return -sortModifier
+    return 0
+  })
 }
--- web/src/components/load-map-dialog.js
+++ web/src/components/load-map-dialog.js
@@ -4,24 +4,67 @@ import { ModalDialog } from './modal-dialog.js'
 
 import styles from './load-map-dialog.css'
 
-export function LoadMapDialog ({open, maps, loadMap, onClose}) {
-  return <ModalDialog title="Load empty map" open={open} onClose={onClose}>
-    <div className={styles.wrapper}>
+export class LoadMapDialog extends React.Component {
+  constructor () {
+    super()
+
+    this.state = {
+      sortBy: 'name',
+      ascending: true,
+    }
+  }
+
+  setSortBy (newSortBy) {
+    const {sortBy, ascending} = this.state
+
+    if (sortBy === newSortBy) {
+      this.setState({
+        ascending: !ascending,
+      })
+    } else {
+      this.setState({
+        sortBy: newSortBy,
+      })
+    }
+  }
+
+  render () {
+    const {open, maps, loadMap, onClose} = this.props
+    const {sortBy, ascending} = this.state
+
+    return <ModalDialog title="Load mission capture" open={open} onClose={onClose}>
       <table className={styles.mapList}>
         <thead className={styles.mapListHeader}>
-        <th>World</th>
+        <HeaderCell setSortBy={this.setSortBy.bind(this)} sortKey={'name'} label={'World'} sortBy={sortBy}
+                    ascending={ascending}/>
         </thead>
         <tbody className={styles.mapListBody}>
-        {maps.sort((a, b) => a.name > b.name)
-          .map(map => <CaptureListItem key={map.name} name={map.name} onClick={() => loadMap(map)}/>)}
+        {sortListBy(maps, sortBy, ascending).map(map =>
+          <CaptureListItem key={map.name} name={map.name} onClick={() => loadMap(map)}/>
+        )}
         </tbody>
       </table>
-      </div>
-  </ModalDialog>
+    </ModalDialog>
+  }
 }
 
 function CaptureListItem ({name, onClick}) {
   return <tr className={styles.mapListItem} onClick={onClick}>
     <td>{name}</td>
   </tr>
+}
+
+function HeaderCell ({sortKey, setSortBy, label, sortBy, ascending}) {
+  const active = sortKey === sortBy
+  const sortSymbol = active ? (ascending ? '▴' : '▾') : ' '
+  return <th className={styles.captureListHeaderItem} onClick={() => setSortBy(sortKey)}>{label} {sortSymbol}</th>
+}
+
+function sortListBy (list, key, ascending) {
+  const sortModifier = ascending ? 1 : -1
+  return [...list].sort((a, b) => {
+    if (a[key] > b[key]) return sortModifier
+    if (a[key] < b[key]) return -sortModifier
+    return 0
+  })
 }
